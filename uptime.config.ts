28bb1f73464221736a4502fb43d04b9fc59fa7eb
pwenxiang51-wq <pwenxiang51@gmail.com>
2025-12-27 01:08:44 +0800

    Update uptime configuration settings

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,4 +1,3 @@
-// Don't edit this line
 import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
@@ -16,52 +15,53 @@ const workerConfig: WorkerConfig = {
       name: '我的博客 (CF Workers)',
       method: 'GET',
       target: 'https://blog.wenxiang0100.workers.dev',
+      timeout: 15000,
     },
     {
       id: 'rn_http',
       name: 'RackNerd - 网站服务',
       method: 'GET',
-      target: 'http://23.94.78.145',
+      target: 'http://23.94.78.145:80',
+      timeout: 15000,
     },
     {
       id: 'rn_ssh',
       name: 'RackNerd - SSH 端口',
       method: 'TCP_PING',
       target: '23.94.78.145:56789',
-      timeout: 5000,
+      timeout: 10000,
     },
     {
       id: 'gcp_http',
       name: 'GCP - 网站服务',
       method: 'GET',
-      target: 'http://35.212.175.192',
+      target: 'http://35.212.175.192:80',
+      timeout: 15000,
     },
     {
       id: 'gcp_ssh',
       name: 'GCP - SSH 端口',
       method: 'TCP_PING',
       target: '35.212.175.192:22',
-      timeout: 5000,
+      timeout: 10000,
     },
   ],
   notification: {
     webhook: {
-      // 自动读取 GitHub Secrets 里的 TG_BOT_TOKEN 和 TG_CHAT_ID
-      url: `https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/sendMessage`,
+      // 7812901048:AAHK0SN28VNfBEUncm-xh58pZRIh3XvUTgc
+      url: 'https://api.telegram.org/bot7812901048:AAHK0SN28VNfBEUncm-xh58pZRIh3XvUTgc/sendMessage',
       method: 'POST',
       payloadType: 'json',
       payload: {
-        chat_id: process.env.TG_CHAT_ID,
+        chat_id: -1003539974591
         text: '$MSG',
       },
     },
     timeZone: 'Asia/Shanghai',
-    gracePeriod: 1, 
+    gracePeriod: 0,
   },
 }
 
-// 维护配置留空，彻底杀掉蓝框
 const maintenances: MaintenanceConfig[] = []
 
-// Don't edit this line
 export { maintenances, pageConfig, workerConfig }
